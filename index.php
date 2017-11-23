ea170fbd5a3721f0f50fe8cee63a14cd22b4ff7f
Timi <timeavereb92@gmail.com>
2017-11-23 21:41:50 +0100

    About menüponthoz tartozó fájlok (ikonok, php, css stb.)

--- index.php
+++ index.php
@@ -1,7 +1,44 @@
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("nov 25, 2017 15:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("counter").innerHTML = days + "<span>n</span> " + hours + "<span>ó</span> "
+    + minutes + "<span>p</span> "/* + seconds + "mp"*/;
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("counter").innerHTML = "Üdvözlünk";
+    }
+}, 1000);
+</script>
 <?php
 
-get_header();
-echo '<div class="polcContentWrapper">
-	 Coming Soon...
+
+echo '
+<link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+	<style>
+	.polcContentWrapper img{position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);}
+	@media all and (max-width:1366px) {div#counter{margin-top:50px!important;}}
+	@media all and (max-width:768px) {.polcContentWrapper img{top:50%;}div#counter{margin-top:10%!important;}}
+	</style>
+	<div class="polcContentWrapper">
+		<div id="counter" style="text-align: center;font-size: 44px;color: #fb5c43;margin-bottom: 20px;margin-top:15%; font-weight: 700; font-family:Titillium Web;"></div>
+		<img src="wp-content/themes/polc/img/plc_november.png">
 	</div>';
-get_footer();
---
+++ layout-handlers/about-layout-handler.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Timi
+ * Date: 2017. 11. 23.
+ * Time: 19:15
+ */
+
+if (!defined("ABSPATH")) {
+    exit();
+}
+
+/**
+ * Class Polc_About_Layout_Handler
+ */
+class Polc_About_Layout_Handler extends Polc_Layout_Handler_Base
+{
+    CONST POLC_LAYOUT = "polc-about";
+    CONST POLC_LAYOUT_NAME = "Rólunk";
+
+
+    public function render()
+    {
+        ?>
+            <div class="plcAboutwrapper">
+                <div class="plcAboutContainer">
+                    <div class="sectionContainer iconLeft">
+                        <div class="part">
+                            <h1>Mi is ez az oldal?</h1>
+                            <h2>A Polc egy mindenki számára elérhető, ingyenes oldal, melyre bárki feltölthet saját irodalmi terméket, vagy böngészheti másokét, legyen az egy saját szereplős regény, vers, vagy akár egy fanfiction.</h2>
+                        </div>
+                        <div class="part">
+                            <div class="icon">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sectionContainer iconRight">
+                        <div class="part">
+                            <h1>Kik vagyunk?</h1>
+                            <h2>Az oldalt jelenleg két ember fejleszti. Egy webfejlesztő és egy designer. Mindketten szabadidejükben foglalkoznak az oldallal, és igyekeznek a legjobb tudásuk szerint a felhasználók rendelkezésére állni. Bármilyen észrevétellel bátran lehet őket keresni az <span>info@polc.eu</span> email címen.</h2>
+                        </div>
+                        <div class="part">
+                            <div class="icon">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sectionContainer iconLeft">
+                        <div class="part">
+                            <h1>Mik a célkitűzéseink?</h1>
+                            <h2>Szeretnénk egy olyan oldalt létrehozni, ahová jó szívvel tér vissza az ember, ahol író és olvasó közvetlen kapcsolatban lehetnek, és ahol nem számít semmi más, csak, hogy jól érezd magad! </h2>
+                        </div>
+                        <div class="part">
+                            <div class="icon">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php
+    }
+}
